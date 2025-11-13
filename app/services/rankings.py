b76d889e721fc4b2148574a9fcb932bec90f8025
Jakub <jakubszmid1@gmail.com>
2025-11-13 19:47:41 +0100

    add team_id to rankings. not adding events, unfortunately seems very limited at the moment :(

--- app/services/rankings.py
+++ app/services/rankings.py
@@ -1,9 +1,7 @@
-from typing import Tuple
 from config.constants import VLR_URL
-from bs4 import BeautifulSoup, ResultSet, Tag, Comment
+from bs4 import Tag, Comment
 from services.vlr_client import VlrClient
-from services.utils import cleanup_text, convert_date_range_format
-from datetime import datetime
+from services.utils import cleanup_text
 
 async def vlr_get_rankings(
         client: VlrClient,
@@ -19,17 +17,23 @@ async def vlr_get_rankings(
     for team in team_tags:
         rank = cleanup_text(team.select_one("div.rank-item-rank-num").text)
         team_info = team.select_one("a.rank-item-team")
+        team_id = team_info["href"].split("/")[-2]
         # find div with class ge-text
-        team_name = ''.join(
-            text for text in
-            team_info
-            .select_one("div.ge-text")
-            .find_all(string=True, recursive=False)
-            # ? Team name div contains comments that serve no purpose
-            if not isinstance(text, Comment)
+        team_name = get_team_name(team_info)
+        team_country = cleanup_text(
+            team_info.select_one("div.rank-item-team-country").get_text()
         )
-        team_name = cleanup_text(team_name)
-        team_country = cleanup_text(team_info.select_one("div.rank-item-team-country").get_text())
-        data.append({"rank": rank, "team_name": team_name, "team_country": team_country})
+        data.append({"rank": rank, "team_name": team_name, "team_country": team_country, "team_id": team_id})
     
     return {"rankings": data}
+
+def get_team_name(team_info: Tag) -> str:
+    name = ''.join(
+        text for text in
+        team_info
+        .select_one("div.ge-text")
+        .find_all(string=True, recursive=False)
+        # ? Team name div contains comments that serve no purpose
+        if not isinstance(text, Comment)
+    )
+    return cleanup_text(name, to_lower=False)
